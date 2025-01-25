bb2b78a5a09cfeccceb7991aae89853a1bfd848f
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-01-25 12:20:10 -0500

    Always show the tab names when scrolling in info display

--- src/components/info-display/InfoDisplay.tsx
+++ src/components/info-display/InfoDisplay.tsx
@@ -68,11 +68,13 @@ const InfoDisplay = ({ floorCode }: Props) => {
   };
 
   return (
-    <div className="max-h-96 w-fit overflow-auto rounded-lg bg-gray-600 px-2 pb-2 text-white shadow-lg">
+    <div className="flex max-h-96 w-fit flex-col rounded-lg bg-gray-600 px-2 pb-2 text-white shadow-lg">
       <ul className="flex text-sm">
         {tabNames.map((tabName, index) => renderTabHeader(tabName, index))}
       </ul>
-      {tabContents[activeTabIndex]()}
+      <div className="flex overflow-y-auto">
+        {tabContents[activeTabIndex]()}
+      </div>
     </div>
   );
 };
